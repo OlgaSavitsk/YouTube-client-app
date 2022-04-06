5ad202229f7b16753f85c0bee08cd02ecd382418
OlgaSavitsk <olbonya.89@mail.ru>
2022-04-06 14:41:40 +0300

    feat: change toggle sort icons

--- src/app/search/components/filter/sort-buttons/sort-buttons.component.ts
+++ src/app/search/components/filter/sort-buttons/sort-buttons.component.ts
@@ -12,14 +12,20 @@ export default class SortButtonsComponent {
   @Output() clickSortDate = new EventEmitter<boolean>();
   @Output() clickSortViews = new EventEmitter<boolean>();
   sortDesk: IDesk = sortDesk;
+  toggleArrowDate: boolean = false;
+  toggleArrowView: boolean = false;
 
   constructor() {}
 
   sortDate() {
     this.clickSortDate.emit();
+    this.toggleArrowDate = true;
+    this.toggleArrowView = false;
   }
 
   sortViews() {
     this.clickSortViews.emit();
+    this.toggleArrowView = true;
+    this.toggleArrowDate = false;
   }
 }
